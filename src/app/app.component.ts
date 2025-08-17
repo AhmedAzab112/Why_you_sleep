8c193b8ab14bda159ba366e689ed2649bcd683ed
AhmedAzab112 <ahmedazab1112@gmail.com>
2025-08-17 16:19:54 +0300

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from "./profile/profile.component";
 import { RouterOutlet } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    RouterOutlet
+],
 })
 export class AppComponent {
-  title = 'why_you_sleep';
+
+  
+
+
+
+
+
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'profile/:Id', component: ProfileComponent },
+];
